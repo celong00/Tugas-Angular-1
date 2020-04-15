c0b5dcd2264db7320268580b0d279d99c36ad2d6
celong00 <michaelalexanderrustan@gmail.com>
2020-04-15 10:48:34 +0800

    Jalan 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Tugas 1 Angular';
+  total=0;
+  output='';
+  cek:boolean;
+  hasil(angka,pangkat){
+  this.total=Math.pow(parseInt(angka),parseInt(pangkat));
+  }
+
+  cekGenapGanjil(total){
+    if(total % 2==0){
+      this.cek=true;
+    }
+    else{
+      this.cek=false;
+    }
+    return this.cek;
+  }
+  
+  pola='';
+  printGenap(angka,pangkat){
+
+    for (var i=pangkat;i<=this.total;i++){
+      for(var j=0;j<i;j++){
+          
+          this.pola+='*';
+
+      }
+      this.pola+='<br>';
+
+    }
+
+
+  }
 }
+ 
