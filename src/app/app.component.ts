ff9a0651260a8bc1077547ee59f18965f0c03c81
celong00 <michaelalexanderrustan@gmail.com>
2020-04-15 11:23:08 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Tugas 1 Angular';
-  total=0;
+  total;
   output='';
-  cek:boolean;
+  cek;
   hasil(angka,pangkat){
   this.total=Math.pow(parseInt(angka),parseInt(pangkat));
   }
 
-  cekGenapGanjil(total){
-    if(total % 2==0){
-      this.cek=true;
+  cekGenapGanjil(){
+    if(this.total % 2==0){
+      this.cek=1;
     }
     else{
-      this.cek=false;
+      this.cek=2;
     }
-    return this.cek;
   }
   
   pola='';
   printGenap(angka,pangkat){
-
+    
+    if(this.cek==1){
+    this.pola='';
     for (var i=pangkat;i<=this.total;i++){
       for(var j=0;j<i;j++){
           
@@ -35,7 +36,32 @@ export class AppComponent  {
       this.pola+='<br>';
 
     }
+    }
 
+  }
+  temp;
+  
+   printGanjil(angka,pangkat){
+    if(this.cek==2) {
+      this.pola='';
+      this.temp=0;
+      for (var i=pangkat;i>=0;i--){
+        for(var j=0;j<i;j++){
+          
+          this.pola+='*';
+
+        }
+        if(this.temp<=angka){
+          for(var k=0;k<this.temp+1;k++){
+            this.pola+='O';
+          }
+        }
+      this.pola+='<br>';
+      this.temp++;
+
+      }
+    }
+    
 
   }
 }
